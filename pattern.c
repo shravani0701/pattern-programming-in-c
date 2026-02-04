a05e86042505a9d4943c396ed5b0145a487e8269
shravani0701 <shravanimaddikuntla18@gmail.com>
2026-02-04 12:38:36 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+int rows,cols;
+printf("enter no of rows:");
+scanf("%d",&rows);
+printf("enter no of cols:");
+scanf("%d",&cols);
+for(int i =1;i<=rows;i++)
+{
+    for(int j=1;j<=cols;j++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+}
+}
